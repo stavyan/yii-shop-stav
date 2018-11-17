ba57e7107aa04629cc61a0c219b74584c4b732d7
stavyan <617946852@qq.com>
2018-11-17 16:47:00 +0800

    添加qq登录功能

--- config/web.php
+++ config/web.php
@@ -77,7 +77,7 @@ if (YII_ENV_DEV) {
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        'allowedIPs' => ['127.0.0.1', '::1', '192.168.125.119'],
+        'allowedIPs' => ['127.0.0.1', '172.27.0.11', '::1', '192.168.125.119'],
     ];
 
     $config['modules']['admin'] = [
