31bb0225b10325bead5829b83a2c99cc72abffad
stavyan <617946852@qq.com>
2018-11-18 23:36:28 +0800

    a

--- vendor/API/comm/inc.php
+++ vendor/API/comm/inc.php
@@ -1,2 +1,2 @@
 <?php die('forbidden'); ?>
-{"appid":"101520029","appkey":"7fca78f82d079ee1c88e6fd93c222ebf","call,"scope":"get_user_info,add_share,list_album,add_album,upload_pic,add_topic,add_one_blog,add_weibo,check_page_fans,add_t,add_pic_t,del_t,get_repost_list,get_info,get_other_info,get_fanslist,get_idolist,add_idol,del_idol,get_tenpay_addr","errorReport":true,"storageType":"file","host":"172.27.0.11","port": "3308","user":"root","password":"qq617946852","database":"yss_shop"}
+{"appid":"101520029","appkey":"7fca78f82d079ee1c88e6fd93c222ebf","call,"scope":"get_user_info","errorReport":true,"storageType":"file","host":"172.27.0.11","port": "3308","user":"root","password":"qq617946852","database":"yss_shop"}
